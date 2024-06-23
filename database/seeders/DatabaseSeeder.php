f17360e46e910963864ba684d10881da8932bf86
Imam Nc <contact.imamnc@gmail.com>
2024-06-23 02:46:30 +0000

    Fixing error on database seeder after refactoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,16 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use Database\Seeders\Collections\BankChannelSeeder;
+use Database\Seeders\Collections\FormTypeSeeder;
+use Database\Seeders\Collections\InvitationSeeder;
+use Database\Seeders\Collections\InvitationTypeSeeder;
+use Database\Seeders\Collections\MusicSeeder;
+use Database\Seeders\Collections\PermissionGroupSeeder;
+use Database\Seeders\Collections\PermissionSeeder;
+use Database\Seeders\Collections\RoleSeeder;
+use Database\Seeders\Collections\ThemeSeeder;
+use Database\Seeders\Collections\UserSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
